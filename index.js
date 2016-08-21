6a5a9d72a2b2b36d42ca389eba539fcb1db10131
Sam Richard <sam@snug.ug>
2016-08-21 14:11:46 -0400

    :bug: Fix label send

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const send = require('./lib/send');
 args().then(arg => {
   return send(arg);
 }).then(message => {
-  console.log(message); // eslint-disable-line no-console
+  console.log(message.message); // eslint-disable-line no-console
   process.exitCode = 0;
 }).catch(e => {
-  console.error(e); // eslint-disable-line no-console
+  console.error(e.message); // eslint-disable-line no-console
   process.exitCode = 1;
 });
