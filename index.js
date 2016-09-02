14f2a84104c57a4638a9ff3b387eb699e1af16c1
Sam Richard <sam@snug.ug>
2016-09-02 09:27:45 -0400

    :new: Add proper CLI

--- index.js
+++ index.js
@@ -2,11 +2,33 @@
 
 'use strict';
 
+const program = require('commander');
+
 const args = require('./lib/args');
 const send = require('./lib/send');
+const travis = require('./lib/travis');
 
-args().then(arg => {
-  return send(arg);
+program
+  .option('-t, --token <token>', 'GitHub Token')
+  .option('-s, --slug <user/repo>', 'GitHub Repo Token (`user/repo`)')
+  .option('-b, --branch <branch>', 'Branch to deploy on')
+  .option('-f, --force', 'Force label generation')
+  .parse(process.argv);
+
+const options = {
+  token: program.token,
+  slug: program.slug,
+  branch: program.branch,
+  force: program.force,
+};
+
+
+args(options).then(opts => {
+  return travis(opts).then(() => {
+    return opts;
+  });
+}).then(opts => {
+  return send(opts);
 }).then(message => {
   console.log(message.message); // eslint-disable-line no-console
   process.exitCode = 0;
